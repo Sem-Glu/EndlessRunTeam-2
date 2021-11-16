b5bbe98a5755e197a281df9f0531c7b10f10cc4a
Sem-Glu <200513@student.glu.nl>
2021-11-16 10:49:09 +0100

    Player

---
+++ Assets/Scripts/Player/PlayerData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Player_Data", menuName = "Player_Data")]
+public class PlayerData : ScriptableObject
+{
+    
+}
