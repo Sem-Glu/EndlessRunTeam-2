d4eb1c67b27caa69ea66a087c496f03db96ff85d
Sem-Glu <200513@student.glu.nl>
2021-11-26 09:17:26 +0100

    Scooooooooooooooooooooooreeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee!!!!!

--- Assets/Scripts/Score/ScoreManager.cs
+++ Assets/Scripts/Score/ScoreManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
@@ -8,6 +9,9 @@ public class ScoreManager : MonoBehaviour
     public ScoreCounter m_score;
     public ScoreData m_data;
     public int CurrentHigh;
+
+    [SerializeField] private TextMeshProUGUI m_scoretext;
+    [SerializeField] private TextMeshProUGUI m_highscoretext;
 
     private void Awake()
     {
@@ -24,6 +28,8 @@ public class ScoreManager : MonoBehaviour
     private void Start()
     {
         CurrentHigh = m_data.HighScore;
+        m_highscoretext.text = "HighScore :" + CurrentHigh.ToString();
+        m_scoretext.text = "Score :" + m_score.MaxScore.ToString();
     }
 
     private void Update()
@@ -31,11 +37,15 @@ public class ScoreManager : MonoBehaviour
         if (m_data.HighScore < CurrentHigh)
         {
             m_data.HighScore = CurrentHigh;
+            m_highscoretext.text = "HighScore :" + CurrentHigh.ToString();
+            m_scoretext.text = "Score :" + m_score.MaxScore.ToString();
         }
 
-        if (m_score.MaxScore < CurrentHigh)
+        if (m_score.MaxScore > CurrentHigh)
         {
-            CurrentHigh = CurrentHigh;
+            CurrentHigh = m_score.MaxScore;
+            m_highscoretext.text = "HighScore :" + CurrentHigh.ToString();
+            m_scoretext.text = "Score :" + m_score.MaxScore.ToString();
         }
     }
 }
